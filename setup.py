98f74deb171353750ab22f49c6326f3d364ee0bd
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-07-01 20:08:08 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import setuptools
 setuptools.setup(
     name='mkfol',
-    version='0.1',
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src'),
+    version='0.2',
+    packages="./src",
     entry_points={
         'console_scripts': ['mkfol=src.mkfol:mkfol']
     },
