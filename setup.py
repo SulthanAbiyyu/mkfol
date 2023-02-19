31c91357657faefb659d8cebef7a7ac4e3cfae6c
Sulthan Abiyyu <zblackjack980@gmail.com>
2023-02-19 12:50:31 +0700

    increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
     name='mkfol',
-    version='1.1.2',
+    version='1.1.3',
     packages=["mkfol"],
     entry_points={
         'console_scripts': ['mkfol=mkfol.mkfol:mkfol']
